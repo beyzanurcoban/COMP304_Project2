a0ce7a888aae28b0bb84f115dd7b11e181c8dd7b
beyzanurcoban <bcoban17@ku.edu.tr>
2021-12-10 15:46:01 +0300

    threads

--- simulation.c
+++ simulation.c
@@ -22,28 +22,16 @@ long start_time;
 long current_time = 0;
 long finish_time;
 struct timeval get_time;
-int vehicle_count = 0;
-int W_count = 1;
-int W_queue[MAX];
-int W_front = 0;
-int W_rear = 0;
 
 // Mutex, Conditional Variables, Semaphores, Locks
-pthread_mutex_t vehicle_count_mutex;
-pthread_mutex_t W_count_mutex;
-pthread_mutex_t cross_mutex; // allows only one vehicle to pass the intersection
-pthread_mutex_t queue_mutex;
-pthread_mutex_t coming;
-pthread_mutex_t leaving;
-pthread_cond_t vTurn_cv;
-pthread_cond_t pTurn_cv;
+
 
 // Function Declarations
 int program_init(int argc, char *argv[]);
 int is_occur(double p);
 int is_finished();
-void *W_func(void* arg);
-void *police_func(void* arg);
+void *lane_func();
+void *police_func();
 int enqueue(int data, int front, int rear, int queue[]);
 int dequeue(int front, int rear, int queue[]);
 
@@ -79,14 +67,9 @@ int program_init(int argc, char *argv[]) {
 	printf("finish time in secs: %ld\n\n", finish_time);
 
 	// Mutex initialization
-	pthread_mutex_init(&vehicle_count_mutex, NULL);
-	pthread_mutex_init(&W_count_mutex, NULL);
-	pthread_mutex_init(&cross_mutex, NULL);
-	pthread_mutex_init(&queue_mutex, NULL);
-	pthread_cond_init(&vTurn_cv, NULL);
-	pthread_cond_init(&pTurn_cv, NULL);
-	pthread_mutex_init(&coming, NULL);
-        pthread_mutex_init(&leaving, NULL);
+
+	return 0;
+	
 }
 
 
@@ -95,15 +78,14 @@ int main(int argc, char *argv[]) {
 	program_init(argc, argv);
 	srand(7); // initialize random number generator seed
 
-	W_queue[0] = 0;
-
 	/* Creating vehicle threads */
 	pthread_t lane_threads[SIZE];
 	pthread_t police;
 
 	pthread_create(&police, NULL, police_func, NULL);
-	pthread_create(&lane_threads[0], NULL, W_func, NULL);
-	//pthread_create(&police, NULL, police_func, NULL);
+	for(int i=0; i<SIZE; i++) {
+		pthread_create(&lane_threads[i], NULL, lane_func, NULL);
+	}
 
 	while(!is_finished()) {
 		gettimeofday(&get_time, NULL);
@@ -114,21 +96,14 @@ int main(int argc, char *argv[]) {
 	}
 	
 	/* Join the worker threads to the master thread */
-	for (int j=0; j<1; j++) {
+	for (int j=0; j<SIZE; j++) {
                 pthread_join(lane_threads[j], NULL);
         }
 
 	pthread_join(police, NULL);
 	
 	/* Destroy mutex */
-	pthread_mutex_destroy(&vehicle_count_mutex);
-	pthread_mutex_destroy(&W_count_mutex);
-	pthread_mutex_destroy(&cross_mutex);
-	pthread_mutex_destroy(&queue_mutex);
-	pthread_cond_destroy(&vTurn_cv);
-        pthread_cond_destroy(&pTurn_cv);
-	pthread_mutex_destroy(&coming);
-        pthread_mutex_destroy(&leaving);
+	
 
 	return 0;
 
@@ -157,32 +132,16 @@ int is_finished() {
 }
 
 /* West lane thread function */
-void *W_func(void* arg) {
-	while(!is_finished()) {
-		pthread_mutex_lock(&coming);
-		pthread_cond_wait(&pTurn_cv, &coming);
-		pthread_mutex_unlock(&coming);
-		printf("West func\n");
-		pthread_cond_signal(&vTurn_cv);
-	}
-
-	//pthread_exit(0);
+void *lane_func() {
+	printf("This is lane function\n");
+	pthread_exit(0);
 }
 
 
 /* Pollice officer thread function */
-void *police_func(void* arg) {
-	while(!is_finished()) {
-		pthread_cond_signal(&pTurn_cv);
-		pthread_sleep(0.01);
-		pthread_mutex_lock(&leaving);
-                pthread_cond_wait(&vTurn_cv, &leaving);
-		pthread_mutex_unlock(&leaving);
-                printf("Police func\n");
-        }
-	//pthread_exit(0);
-
-
+void *police_func() {
+	printf("This is police function\n");
+	pthread_exit(0);
 }
 
 /* Queue Operations */
